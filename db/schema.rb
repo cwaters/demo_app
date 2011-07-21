2ce5ce63c4cc185bb2d1a1b2ca618f2f00e3ec87
Your Name <christianwaters1993@gmail.com>
2011-07-21 09:30:20 -0400

    Initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110719194732) do
+ActiveRecord::Schema.define(:version => 20110721124307) do
+
+  create_table "people", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
